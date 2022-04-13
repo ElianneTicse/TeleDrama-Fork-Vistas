37f86980a181720c883d6308c0fd7510ef868c65
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-04-13 17:43:15 -0500

    Initial commit

---
+++ src/main/java/pe/edu/pucp/teledrama/TeleDramaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.teledrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleDramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeleDramaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/teledrama/TeleDramaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.teledrama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleDramaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
