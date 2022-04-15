2c244f6f62722c6afa7d1903a1dabd3ec54672b5
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-04-14 23:42:00 -0500

    Estructura MVC

---
+++ src/main/java/pe/edu/pucp/teledrama/controllers/OperadorController.java
@@ -0,0 +1,7 @@
+package pe.edu.pucp.teledrama.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OperadorController {
+}
---
+++ src/main/java/pe/edu/pucp/teledrama/entity/Obra.java
@@ -0,0 +1,9 @@
+package pe.edu.pucp.teledrama.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "obra")
+public class Obra {
+}
